130ab548538a45594a8a0344796e8d865a725823
evenilson <evenilsonguerreiro@gmail.com>
2026-03-26 17:44:16 -0300

    Ambiente configurado com sucesso: MVC, Dotenv e Neon

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const userRoutes = require('./src/router/userRoutes');
+const userRoutes = require('./router/userRoutes');
 
 const {neon} = require('@neondatabase/serverless');
 
